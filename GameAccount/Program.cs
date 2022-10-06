ce1864c2dfe391feab86e4ab7f5294a974f897e7
RezenkovD <rezenkovdmitro@gmail.com>
2022-10-06 09:55:50 +0300

    Add functional in methods and correct rating calculation

--- GameAccount/Program.cs
+++ GameAccount/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace GameAccount
+﻿using System;
+using System.Collections.Generic;
+
+namespace GameAccount
 {
     internal class Program
     {
@@ -10,21 +13,36 @@
         {
             public string UserName { get; }
             public int GamesCount { get; }
-            public int CurrentRating { get; }
+
+            public int CurrentRating
+            {
+                get
+                {
+                    int currentRating = 0;
+                    foreach (var item in allRatingCalculations)
+                    {
+                        currentRating += item.Rating;
+                    }
+                    return currentRating;
+                }
+            }
 
             public void StartGame(int rating, string status)
             {
-                
+                var startGame = new RatingCalculation(rating, status, "null");
+                allRatingCalculations.Add(startGame);
             }
 
             public void WinGame(string opponentName, int rating)
             {
-                
+                var winGame = new RatingCalculation(rating, "win game", opponentName);
+                allRatingCalculations.Add(winGame);
             }
 
             public void LoseGame(string opponentName, int rating)
             {
-                
+                var loseGame = new RatingCalculation(rating, "win game", opponentName);
+                allRatingCalculations.Add(loseGame); 
             }
 
             public void GetStats()
@@ -36,8 +54,11 @@
             {
                 UserName = userName;
                 GamesCount = 0;
-                CurrentRating = 0;
+                StartGame(0, "start game");
             }
+
+            private List<RatingCalculation> allRatingCalculations = new List<RatingCalculation>();
+            
         }
 
         public class RatingCalculation
