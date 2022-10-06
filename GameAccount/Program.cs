9156467aa6f91b5719d42a539037fde571813326
RezenkovD <rezenkovdmitro@gmail.com>
2022-10-06 09:38:00 +0300

    Create empty GameAccount

--- GameAccount/Program.cs
+++ GameAccount/Program.cs
@@ -5,5 +5,39 @@
         public static void Main(string[] args)
         {
         }
+
+        public class GameAccount
+        {
+            public string UserName { get; }
+            public int GamesCount { get; }
+            public int CurrentRating { get; }
+
+            public void StartGame(int rating, string status)
+            {
+                
+            }
+
+            public void WinGame(string opponentName, int rating)
+            {
+                
+            }
+
+            public void LoseGame(string opponentName, int rating)
+            {
+                
+            }
+
+            public void GetStats()
+            {
+                
+            }
+
+            public GameAccount(string userName)
+            {
+                UserName = userName;
+                GamesCount = 0;
+                CurrentRating = 0;
+            }
+        }
     }
 }
