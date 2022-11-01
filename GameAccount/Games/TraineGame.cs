71d831a0c5c9a5aa0aac870f3bf8d68f95f211ef
RezenkovD <rezenkovdmitro@gmail.com>
2022-11-01 21:53:18 +0200

    Transfer this in methods

--- GameAccount/Games/TraineGame.cs
+++ GameAccount/Games/TraineGame.cs
@@ -16,13 +16,13 @@ namespace GameAccount
             RandomChoice = rnd.Next(0, 101);
             if (RandomChoice <= 50)
             {
-                UserOne.WinGame(UserTwo.UserName, 0);
-                UserTwo.LoseGame(UserOne.UserName, 0);
+                UserOne.WinGame(UserTwo.UserName, this);
+                UserTwo.LoseGame(UserOne.UserName, this);
             }
             else
             {
-                UserTwo.WinGame(UserOne.UserName, 0);
-                UserOne.LoseGame(UserTwo.UserName, 0);
+                UserTwo.WinGame(UserOne.UserName, this);
+                UserOne.LoseGame(UserTwo.UserName, this);
             }
         }
     }
