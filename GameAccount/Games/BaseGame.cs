9668d4baf9dad6257190fc1a6a6385edb0dd1e42
RezenkovD <rezenkovdmitro@gmail.com>
2022-11-01 21:47:24 +0200

    Add field Rating and add new constructor

--- GameAccount/Games/BaseGame.cs
+++ GameAccount/Games/BaseGame.cs
@@ -2,15 +2,22 @@ namespace GameAccount
 {
     public abstract class BaseGame
     {
+        public int Rating { get; }
         protected readonly BaseGameAccount UserOne;
         protected readonly BaseGameAccount UserTwo;
 
+        protected BaseGame(BaseGameAccount userOne, BaseGameAccount userTwo, int rating)
+        {
+            UserOne = userOne;
+            UserTwo = userTwo;
+            Rating = rating;
+        }
         protected BaseGame(BaseGameAccount userOne, BaseGameAccount userTwo)
         {
             UserOne = userOne;
             UserTwo = userTwo;
+            Rating = 0;
         }
-
         public abstract void PlayGame();
     }
 }
