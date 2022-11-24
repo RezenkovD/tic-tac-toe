3c2298b91a8e19c1b846458b90b055f8a759252a
RezenkovD <rezenkovdmitro@gmail.com>
2022-11-24 18:47:44 +0200

    Update Program.cs

--- GameAccount/Program.cs
+++ GameAccount/Program.cs
@@ -1,39 +1,42 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Npgsql;
 
 namespace GameAccount
 {
     internal class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             var Riezienkov = new BaseGameAccount("Riezienkov");
-            var Nizhynets = new SuperGameAccount("Nizhynets");
-            var Lomachenko = new ComboGameAccount("Lomachenko");
-            
+            var Nizhynets = new ComboGameAccount("Nizhynets");
+            var Lomachenko = new SuperGameAccount("Lomachenko");
             var RiezienkovVsLomachenko = GameFactory.GetStandartGame(Riezienkov, Lomachenko, 20);
             var NizhynetsVsRiezienkov = GameFactory.GetTrainetGame(Nizhynets, Riezienkov);
             var LomachenkoVsNizhynets = GameFactory.GetStandartGame(Nizhynets, Lomachenko, 10);
             
             RiezienkovVsLomachenko.PlayGame();
-            RiezienkovVsLomachenko.PlayGame();
-            RiezienkovVsLomachenko.PlayGame();
-            RiezienkovVsLomachenko.PlayGame();
-            NizhynetsVsRiezienkov.PlayGame();
             NizhynetsVsRiezienkov.PlayGame();
             LomachenkoVsNizhynets.PlayGame();
-            LomachenkoVsNizhynets.PlayGame();
-            LomachenkoVsNizhynets.PlayGame();
-            LomachenkoVsNizhynets.PlayGame();
             
-            Console.WriteLine(Riezienkov.GetStats());
-            Console.WriteLine(Nizhynets.GetStats());
-            Console.WriteLine(Lomachenko.GetStats());
+            // var NizhynetsVsLomachenko = GameFactory.GetTicTacToe(Nizhynets, Lomachenko, 20);
+            // NizhynetsVsLomachenko.PlayGame();
             
-            var NizhynetsVsLomachenko = GameFactory.GetTicTacToe(Nizhynets, Lomachenko, 20);
-            NizhynetsVsLomachenko.PlayGame();
+            await Riezienkov.WriteStats();
+            await Nizhynets.WriteStats();
+            await Lomachenko.WriteStats();
             
-            Console.WriteLine(Nizhynets.GetStats());
-            Console.WriteLine(Lomachenko.GetStats());
+            Console.Write("username\t");
+            Console.Write("currentrating\t");
+            Console.Write("status\t\t");
+            Console.Write("opponentname\t");
+            Console.Write("rating\t\t");
+            Console.Write("gameindex\t");
+            Console.Write("typegame\t"); 
+            Console.WriteLine();
+            
+            var readDataBase = new DataBase.DataBase();
+            await readDataBase.ReadDataBase();
         }
     }
 }
